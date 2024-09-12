324b0122da522d2a3e44fe8a063f3699206818bc
Raymond Teng Toh Zi <raintaroteng@gmail.com>
2024-09-12 13:05:51 +0800

    add details button for user order

--- src/main/java/kjry/ecommerce/controller/UserOrderHistoryController.java
+++ src/main/java/kjry/ecommerce/controller/UserOrderHistoryController.java
@@ -1,5 +1,6 @@
 package kjry.ecommerce.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,12 +10,17 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import kjry.ecommerce.App;
 import kjry.ecommerce.dtos.OrdersDTO;
@@ -36,6 +42,9 @@ public class UserOrderHistoryController implements Initializable {
 
     @FXML
     private TableView<OrdersDTO> orderTableView;
+
+    @FXML
+    private TableColumn<OrdersDTO, Button> actionTableColumn;
 
     @FXML
     private TableColumn<OrdersDTO, OrdersDTO.StatusDTO> statusTableColumn;
@@ -74,7 +83,47 @@ public class UserOrderHistoryController implements Initializable {
                 return new SimpleObjectProperty<>(String.format("%.2f", OrderService.calculateBill(param.getValue())));
             }
         });
+        actionTableColumn.setCellFactory(column -> new TableCell<OrdersDTO, Button>() {
+            private final Button button = new Button("Details");
+
+            {
+                button.setOnAction(event -> {
+                    OrdersDTO order = getTableView().getItems().get(getIndex());
+                    System.out.println("Action button clicked for order: " + order.getId());
+                    orderPromptDialog(order, false);
+                });
+            }
+
+            @Override
+            protected void updateItem(Button item, boolean empty) {
+                super.updateItem(item, empty);
+                setGraphic(empty ? null : button);
+            }
+        });
         orderTableView.setItems(list);
+    }
+
+    private boolean orderPromptDialog(OrdersDTO order, boolean editable) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/AdminOrderInfoAlert.fxml"));
+            VBox dialogContent = loader.load();
+
+            AdminOrderInfoDialogController controller = loader.getController();
+            controller.setOrder(order, !editable);
+
+            Stage dialogStage = new Stage();
+            controller.setParentStage(dialogStage);
+            dialogStage.initModality(Modality.APPLICATION_MODAL);
+            dialogStage.setTitle("Order Information");
+            dialogStage.setResizable(false);
+            dialogStage.setScene(new Scene(dialogContent));
+            dialogStage.showAndWait();
+            return controller.isCreate();
+        } catch (IOException ex) {
+            System.out.println("Error when loading AdminOrderInfoAlert.fxml");
+            ex.printStackTrace();
+            return false;
+        }
     }
 
 }
